f5384ab836a6560636edc0205499d2915a5bb474
Radniel Padron <radnielpadron@gmail.com>
2020-01-21 16:17:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   # your code here
-  # arr = []
-  # i = 0
-  # while i < array.length do
-  #   arr.push(array[i**])
-  # end
-  # arr
+  arr = []
+  i = 0
+  while i < array.length do
+    arr << 
+  end
+  arr
 end
